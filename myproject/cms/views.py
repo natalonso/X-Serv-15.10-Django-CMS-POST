430dccad93499fc78b7d20e70cd8dbd6ee411b18
Natalia Alonso Arteaga <nalonso@alpha17.aulas.gsyc.urjc.es>
2018-04-18 18:06:14 +0200

    primera version, todavia no edita, pero tiene plantilla

---
+++ myproject/cms/views.py
@@ -0,0 +1,106 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.template.loader import get_template
+from django.template import Context
+from .models import Page
+
+formulario = """
+<form action="" method="POST">
+    Name: <input type="text" name="name"><br>
+    Page: <input type="text" name="page"><br>
+    <input type="submit" value="Enviar">
+</form>
+"""
+@csrf_exempt
+def home_anotated(request): #PAGINA PRINCIPAL
+
+    if request.method == 'POST':
+        newpage= Page(name=request.POST['name'], page=request.POST['page'])
+        newpage.save()
+
+    if request.user.is_authenticated():
+        logged = 'Logged in as: ' + request.user.username + "<br><a href='/logout'>Logout</a><br><br>"
+        permiso = True
+    else:
+        logged = "Not logged in. <br><a href='/login'>Login</a><br><br>"
+        permiso = False
+
+    lista = Page.objects.all()
+    salida = "<h4>" + logged + "</h4><br>"
+    salida += "<h1>Bienvenido al servidor CMS, estas son las paginas disponibles hasta el momento: </h1><br>"
+    salida += "<ul>"
+    for pagina in lista:
+        salida += '<li><h3><a href=' + str(pagina.name) + '>' + pagina.name + '</a></h3>'
+    salida += "</ul>"
+
+    if permiso == True: #estas logeado
+        salida += formulario
+        template = get_template("terrafirma/index.html")
+        c =({'title' : "SERVIDOR CMS", 'contenido' : salida})
+        return HttpResponse(template.render(c))
+    else: #no estas logeado
+        template = get_template("terrafirma/index.html")
+        c =({'title' : "SERVIDOR CMS", 'contenido' : salida})
+        return HttpResponse(template.render(c))
+
+
+@csrf_exempt
+def home(request): #PAGINA PRINCIPAL
+
+    if request.method == 'POST':
+        newpage= Page(name=request.POST['name'], page=request.POST['page'])
+        newpage.save()
+
+    if request.user.is_authenticated():
+        logged = 'Logged in as: ' + request.user.username
+        permiso = True
+    else:
+        logged = 'Not logged in.'
+        permiso = False
+
+    lista = Page.objects.all()
+    salida = "Bienvenido al servidor CMS, estas son las paginas disponibles hasta el momento: "
+    salida += "<ul>"
+    for pagina in lista:
+        salida += '<li><a href=' + str(pagina.name) + '>' + pagina.name + '</a>'
+    salida += "</ul>"
+
+    if permiso == True: #estas logeado
+        return HttpResponse(logged + '<br><a href= "/logout">Logout</a><br><br>' + salida + formulario )
+    else: #no estas logeado
+        return HttpResponse(logged + '<br><a href= "/login">Login</a><br><br>' + salida)
+
+@csrf_exempt
+def pagina(request, pagina):
+
+    lista = Page.objects.all()
+    for elemento in lista:
+        if elemento.name == pagina:
+            salida = elemento.page
+            break
+        else:
+            salida = None
+    if salida == None:
+        return HttpResponse('Lo sentimos. La pagina no esta en la base de datos por el momento.')
+    else:
+        return HttpResponse(salida)
+
+
+@csrf_exempt
+def edit(request, pagina):
+
+    lista = Page.objects.all()
+    for elemento in lista:
+        if elemento.name == pagina:
+            salida = elemento.page
+            break
+        else:
+            salida = None
+    if salida == None:
+        return HttpResponse('Lo sentimos. La pagina no esta en la base de datos por el momento.')
+    else:
+        #FUNCIONALIDAD DEL FORMULARIO. SI EL RECURSO EXISTE SE MANDA EL FORMULARIO.
+        #SE ENVIARA UN POST Y SE EDITARA EL RECURSO EN LA BASE DE DATOS.
+        #MODIFICAR A PARTIR DE AQUI.
+        return HttpResponse(formulario)
